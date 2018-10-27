8fab566ffdff97dd98a5c3292702d66f6ba8e5bf
nics <nicshackle@gmail.com>
2018-10-27 21:49:25 +0200

    initial

---
+++ electron.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({fullscreen: true});
+  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
